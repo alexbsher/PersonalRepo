921195f97b71ed92996339b4ba12a9567fc4ed63
Alex Sher <alexander.sher@berkshiregrey.com>
2020-03-17 16:00:08 +0000

    Adds ability to copy rather than link ubuntu setup

--- Ubuntu_Config/setup_bash.sh
+++ Ubuntu_Config/setup_bash.sh
@@ -6,44 +6,52 @@ if [ $CUR_DIR_BASE != "Ubuntu_Config" ]; then
   exit
 fi
 
+cmd='ln -s'
+
+while getopts c o; do
+  case $o in
+    (c) cmd=cp
+  esac
+done
+
 if [ -f ~/.bashrc ]; then
   rm ~/.bashrc
 elif [ -h ~/.bashrc ]; then
   unlink ~/.bashrc
 fi
-ln -s $CUR_DIR/.bashrc ~/.bashrc
+$cmd $CUR_DIR/.bashrc ~/.bashrc
 
 if [ -f ~/.vimrc ]; then
   rm ~/.vimrc
 elif [ -h ~/.vimrc ]; then
   unlink ~/.vimrc
 fi
-ln -s $CUR_DIR/.vimrc ~/.vimrc
+$cmd $CUR_DIR/.vimrc ~/.vimrc
 
 if [ -f ~/.bash_functions ]; then
   rm ~/.bash_functions
 elif [ -h ~/.bash_functions ]; then
   unlink ~/.bash_functions
 fi
-ln -s $CUR_DIR/.bash_functions ~/.bash_functions
+$cmd $CUR_DIR/.bash_functions ~/.bash_functions
 
 if [ -f ~/.bash_aliases ]; then
   rm ~/.bash_aliases
 elif [ -h ~/.bash_aliases ]; then
   unlink ~/.bash_aliases
 fi
-ln -s $CUR_DIR/.bash_aliases ~/.bash_aliases
+$cmd $CUR_DIR/.bash_aliases ~/.bash_aliases
 
 if [ -f ~/.git-completion.bash ]; then
   rm ~/.git-completion.bash
 elif [ -h ~/.git-completion.bash ]; then
   unlink ~/.git-completion.bash
 fi
-ln -s $CUR_DIR/git-completion.bash ~/.git-completion.bash
+$cmd $CUR_DIR/git-completion.bash ~/.git-completion.bash
 
 if [ -f ~/.git-prompt.sh ]; then
   rm ~/.git-prompt.sh
 elif [ -h ~/.git-prompt.sh ]; then
   unlink ~/.git-prompt.sh
 fi
-ln -s $CUR_DIR/git-prompt.sh ~/.git-prompt.sh
+$cmd $CUR_DIR/git-prompt.sh ~/.git-prompt.sh
